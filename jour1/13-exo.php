2716b45d2912a443d836df54870544dd01cfc291
Malik H <malik.h@webdevpro.net>
2023-01-30 17:00:25 +0100

    fin jour 1

--- jour1/13-exo.php
+++ jour1/13-exo.php
@@ -1,18 +1,15 @@
-<?php 
-// http://localhost/php-initiation/jour1/13-exo.php
+<?php  // http://localhost/php-initiation/jour1/13-exo.php
 /*
 4 x 2 = 8
 4 x 3 = 12
 4 x 4 = 16
 4 x 5 = 20
 4 x 6 = 24
-*/
-?>
+*/ ?>
 <h1>solution 1</h1>
 <?php for($i=2 ; $i <= 6 ; $i++) : ?>
     <p><?php echo "4 x $i = " . $i * 4  ?></p>
 <?php endfor ?>
-
 <h1>solution 2</h1>
 <?php 
 for($i=2 ; $i <= 6 ; $i++) {
