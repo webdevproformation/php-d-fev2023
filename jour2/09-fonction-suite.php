727cc6c25c4994538756ade7a1e6e042abb56516
Malik <malik.h@webdevpro.net>
2023-01-31 15:53:51 +0100

    énoncé

--- jour2/09-fonction-suite.php
+++ jour2/09-fonction-suite.php
@@ -33,3 +33,15 @@ $total = ltrim(information(1, false, "element1"));
 
 // essayer de réduire le nombre d'erreurs / coquille au moment de l'exécution du code 
 // comme une petite documentation 
+
+// créer le fichier 10-exo.php 
+// créer une fonction qui s'appelle aireCercle 
+// dispose de 2 paramètres rayon chiffre à virgule et unite chaine de caractère
+// contient les instructions suivantes 
+// créer une phrase "un cercle de rayon rayon a une aire de " Pi * rayon puissance 2 + unite ²
+// retourner la variable phrase (de type string)
+
+// exécuter cette fonction avec les paramètres suivants 
+// 30  ,cm
+// 12.4 , km  
+
