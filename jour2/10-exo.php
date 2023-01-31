07107b34a3f5f43d9281cfff13e74de7cbe5f941
Malik <malik.h@webdevpro.net>
2023-01-31 16:31:19 +0100

    note en +

--- jour2/10-exo.php
+++ jour2/10-exo.php
@@ -1,12 +1,13 @@
 <?php 
-declare(strict_types=1);
+declare(strict_types=1); // permet de dire à PHP de respecter de manière stricte les types ajoutés dans la signature des fonctions 
 // http://localhost/php-initiation/jour2/10-exo.php
 
 // signature de la fonction 
 function aireCercle( float $rayon , string $unite ) :string{
 
     $phrase = "un cercle de rayon $rayon a une aire de " . 3.14 * $rayon ** 2 . " $unite <sup>2</sup>" ;
-    return "<p>$phrase</p>";
+    return "<p>$phrase</p>"; // return permet de récupérer 
+    // un traitement réalisé dans fonction 
 
 }
 
