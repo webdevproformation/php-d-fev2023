cf451d6ea1a8564710aa73c58065b2f9cc83b4d5
Malik <malik.h@webdevpro.net>
2023-01-31 14:37:00 +0100

    pramètre de fonction

--- jour2/07-fonction-suite.php
+++ jour2/07-fonction-suite.php
@@ -6,9 +6,7 @@ function calcul(){
     $b = 3 ;
     echo "<p>" . $a + $b ."</p>" ;
 }
-
 calcul();
-
 // attention en PHP il n'est pas possible de créer deux fois la fonction dans un fichier / projet
 // Fatal error: Cannot redeclare calcul() 
 function calcul2(){ 
@@ -17,13 +15,20 @@ function calcul2(){
     echo "<p>" . $a + $b ."</p>" ;
 }
 calcul2() ;
-
 // pour pouvoir rendre les valeurs à calculer dynanimque => ajouter des paramètres à la fonction 
-
 function calcul3($a, $b){
     echo "<p>" . $a + $b ."</p>" ;
 }
 // définir la valeur de $a et $b au moment de l'appel de la fonction
-
 calcul3(12, 15) ; // $a = 12 et $b = 15
 calcul3(33, -20) ; // $a = 33 et $b = -20
+
+// type hinting, à partir de la version 7 PHP il est possible de donner les types de paramètres
+
+function calcul4(int $a, int $b){ // type hinting
+    echo "<p>" . $a + $b ."</p>" ;
+}
+
+calcul4(12, 15) ;
+calcul4("text", 15) ; // Fatal error: Uncaught TypeError: calcul4(): Argument #1 ($a) must be of type int, string given,
+
