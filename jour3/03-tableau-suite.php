9d9e08f556f75a92576a4872cfd83b141bd226ae
Malik H <malik.h@webdevpro.net>
2023-02-01 12:28:04 +0100

    enoncé

--- jour3/03-tableau-suite.php
+++ jour3/03-tableau-suite.php
@@ -49,3 +49,26 @@ foreach($etudiants as $tutu){
 for($i = 0 ; $i < count($etudiants) ; $i++){
     echo "<p>{$etudiants[$i]["nom"]} débute sur {$etudiants[$i]["competences"][1]} et maîtrise {$etudiants[$i]["competences"][0]}</p>";
 }
+
+// créer le fichier 04-exo.php 
+// créer une variable qui s'appelle villes qui est de type tableau indexé
+// ce tableau contient 2 tableaux associatifs 
+[
+    nom Paris
+    nbHabitant : 5_000_000
+    arrondissements : 20 ,
+    monuments [ "sacré coeur" , "tour eiffel" ]
+]
+
+[
+    nom Marseille 
+    nbHabitant : 3_000_000
+    arrondissements 15 
+    monuments ["vieux port" , "vélodrome"]
+]
+
+/* écrire dans le navigateur les phrases suivantes 
+
+lorsque je vais à Paris je vais visiter deux monuments : sacré coeur et  tour eiffel
+lorsque je vais à Marseille je vais visiter deux monuments : vieux port et vélodrome */
+
