b353f0b701baffa2adae323d4f78a45d5fb34adb
Malik H <malik.h@webdevpro.net>
2023-02-02 16:36:40 +0100

    correction

---
+++ jour4/08-data.php
@@ -0,0 +1,7 @@
+<?php 
+// http://localhost/php-initiation/jour4/08-exo.php
+$produits = [
+    [ "nom" => "Playstation" , "prix" => 300.5 ],
+    [ "nom" => "Nintendo DS" , "prix" => 200 ],
+    [ "nom" => "Xbox" , "prix" => 320 ]
+   ];
