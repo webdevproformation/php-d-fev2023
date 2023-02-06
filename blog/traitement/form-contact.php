50c6fa8dfff8613ace21a990d703623155f4fb8a
Malik H <malik.h@webdevpro.net>
2023-02-06 11:44:21 +0100

    fin du site

---
+++ blog/traitement/form-contact.php
@@ -0,0 +1,37 @@
+<?php 
+// http://localhost/php-initiation/blog/traitement/form-contact.php
+session_start();
+
+$erreurs = [];
+// si un des deux champs est vide => ajouter une erreur 
+if(empty($_POST["email"]) || empty($_POST["commentaire"])){
+    array_push($erreurs , "veuillez compléter les deux champs");
+}
+
+// est ce que l'email est un email avec une forme valide 
+if(!filter_var($_POST["email"] , FILTER_VALIDATE_EMAIL)){
+    array_push($erreurs , "l'email transmis n'est pas valide");
+}
+
+// le champ commentaire doit contenir au mimnimum 4 lettres et maximum 1000 lettres
+if(strlen($_POST["commentaire"]) <= 4 || strlen($_POST["commentaire"]) > 1000){
+    array_push($erreurs , "le champ commentaire doit contenir entre 4 et 1000 lettres");
+}
+
+$_SESSION["form"] = $_POST ; // permet de retourner les valeurs dans le formulaire
+
+if(count($erreurs) === 0){ // afficher un message sous le formulaire
+    $_SESSION["message"] = [
+        "alert" => "success",
+        "info" => "merci !!!"
+    ];
+    $_SESSION["form"] = [];
+}else {
+    $_SESSION["message"] = [
+        "alert" => "danger",
+        "info" => $erreurs 
+    ];
+}
+header("Location: http://localhost/php-initiation/blog/index.php?page=contact"); 
+// rediriger vers le formulaire
+exit ; 
--- blog/vue/contact.php
+++ blog/vue/contact.php
@@ -5,16 +5,35 @@ login
 password  
  -->
 <h1>Nous contacter</h1>
-<form action="" method="POST">
+<form action="http://localhost/php-initiation/blog/traitement/form-contact.php" method="POST">
     <div class="mb-3">
         <label for="email">votre email</label>
-        <input type="email" name="email" class="form-control" id="email" required placeholder="saisir votre email">
+        <input type="email" name="email" class="form-control" id="email" required 
+        placeholder="saisir votre email" 
+        value="<?php echo !empty($_SESSION["form"]["email"]) ? $_SESSION["form"]["email"] : "" ?>">
     </div>
     <div class="mb-3">
         <label for="commentaire">votre commentaire</label>
-        <textarea name="commentaire" id="commentaire" cols="30" rows="4" class="form-control" placeholder="saisir votre commentaire" required></textarea>
+        <textarea name="commentaire" id="commentaire" cols="30" rows="4" class="form-control" placeholder="saisir votre commentaire" required><?php echo !empty($_SESSION["form"]["commentaire"]) ? $_SESSION["form"]["commentaire"] : "" ?></textarea>
     </div>
     <div class="mb-3">
         <input type="submit" class="btn btn-outline-primary btn-sm">
     </div>
 </form>
+<?php if(!empty($_SESSION["message"])) : ?>
+    <?php if($_SESSION["message"]["alert"] === "success") : ?>
+        <div class="alert alert-success">
+            <?php echo $_SESSION["message"]["info"] ?>
+        </div>
+    <?php else : ?>
+        <div class="alert alert-danger">
+            <?php foreach($_SESSION["message"]["info"] as $m) : ?>
+                <?php echo $m ?> <br>
+            <?php endforeach ?>
+        </div>
+    <?php endif ?>
+<?php endif ?>
+<?php if(!empty($_SESSION["message"])){
+    $_SESSION["message"] = [];
+}
+?>
