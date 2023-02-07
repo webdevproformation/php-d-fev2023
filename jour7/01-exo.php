79a410ddc80762aad5722f60362393c2f2b43fea
Malik <malik.h@webdevpro.net>
2023-02-07 10:20:28 +0100

    correction

---
+++ jour7/01-exo.php
@@ -0,0 +1,23 @@
+<?php 
+// http://localhost/php-initiation/jour7/01-exo.php 
+
+class Immeuble {
+    public string $adresse = "102 rue de Lille" ;
+    public array $habitants = ["Alain" , "Céline" , "Pierre" , "Zorro"];
+    public bool $etatAscenseur = false ;
+    public function infoMaintenance () :string{
+        if($this->etatAscenseur  === false){
+            return "l'ascenseur est en panne" ;
+        }else {
+            return "l'ascenseur est en fonctionnel" ;
+        }
+    }
+    public function description() : string{
+        // Alain Zorro et Céline habitent au 102 rue de Lille 
+        return "{$this->habitants[0]} {$this->habitants[3]} et {$this->habitants[1]} habitent au {$this->adresse}";
+    }
+}
+
+$immeuble = new Immeuble() ; // créer l'objet $immeuble 
+echo $immeuble->infoMaintenance() ."<br>"; // ne pas oublier les () à la fin sinon c'est une propriété
+echo $immeuble->description() . "<br>";
