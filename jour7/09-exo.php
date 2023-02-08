9ac668f9ef087522e98b628082b0cd584ede92e8
Malik H <malik.h@webdevpro.net>
2023-02-08 10:11:50 +0100

    enoncé

--- jour7/09-exo.php
+++ jour7/09-exo.php
@@ -3,16 +3,20 @@
 
 $connexion = new PDO("sqlite:./06-bdd.db");
 
-/* $connexion->query("
-    UPDATE auteur SET dt_creation = '2012-01-01' WHERE id = 1 ;
-"); */
-
-// echo "mise à jour effectuée" ;
+$connexion->query("
+    UPDATE auteur SET prenom = 'titi' WHERE id = 1 ;
+"); 
 
 $connexion->query("
+    UPDATE auteur SET prenom = 'tata' WHERE id = 4 ;
+"); 
+
+echo "mise à jour effectuée" ;
+
+/* $connexion->query("
     DELETE FROM auteur WHERE id >= 5 
 ");
 
-echo "suppression effectuée" ;
+echo "suppression effectuée" ; */
 
 // www => Apache => php => fichier => traiter => si ok "suppression effectuée" / si ko
