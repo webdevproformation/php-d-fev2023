cde4faf8d2d54463a056c3d43099f482ccf0c7a9
Malik H <malik.h@webdevpro.net>
2023-02-08 09:36:32 +0100

    correction

---
+++ jour7/08-exo.php
@@ -0,0 +1,35 @@
+<?php 
+// http://localhost/php-initiation/jour7/08-exo.php
+
+$connexion = new PDO("sqlite:./06-bdd.db");
+
+/* $connexion->query("
+    INSERT INTO auteur 
+    ()
+    VALUES 
+    () ,
+    () ,
+    () ,
+    () ;
+"); */
+
+$connexion->query("
+    INSERT INTO auteur 
+       ( prenom , nom , status , dt_creation )
+    VALUES 
+        ('Victor', 'Hugo' , TRUE , '2023-01-01') ,
+        ('George', 'Sand' , TRUE , '2023-02-01') ,
+        ('Jean Jacques' , 'Rousseau' , FALSE , '2012-01-15') ,
+        ('Arthur' , 'Rimbaud' , TRUE , '2023-01-01') ;
+");
+
+
+// CREATE TABLE
+// ALTER 
+// DROP TABLE auteur ; 
+
+// ligne dans une table
+// SELECT 
+// INSERT INTO
+// UPDATE
+// DELETE 
