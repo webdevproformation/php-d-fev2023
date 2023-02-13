716c6f4ec70374d484f689f7e5684abfec306bb5
Malik <malik.h@webdevpro.net>
2023-02-13 14:29:51 +0100

    le formulaire ajout de page

--- projet/vue/privee/gestion-page-form.php
+++ projet/vue/privee/gestion-page-form.php
@@ -19,5 +19,35 @@
 
             bonus (ensemble) => transformer le champ textarea rich via la librairie tinyMCE (ensemble)
         -->
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="titre">titre de l'article</label>
+                <input type="text" class="form-control" 
+                    name="titre" placeholder="titre article" id="titre">
+            </div>
+            <div class="mb-3">
+                <label for="slug">slug de l'article</label>
+                <input type="text" class="form-control" 
+                    name="slug" placeholder="slug article" id="slug">
+            </div>
+            <div class="mb-3">
+                <label for="contenu">contenu article</label>
+                <textarea name="contenu" id="contenu" class="form-control" rows="5" 
+                    placeholder="contenu article"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="image">image</label>
+                <input type="text" class="form-control" 
+                    name="image" placeholder="url de l'image" id="image">
+            </div>
+            <div class="mb-3">
+                <label for="auteur">auteur</label>
+                <input type="text" class="form-control" 
+                    name="auteur" placeholder="auteur" id="auteur">
+            </div>
+            <div class="mb-3">
+                <input type="submit" class="btn btn-success">
+            </div>
+        </form>
     </div>
 </section>
