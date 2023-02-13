b8802579c280b7adb832e8cdca31efe051a70017
Malik <malik.h@webdevpro.net>
2023-02-13 13:50:12 +0100

    énoncé

--- projet/vue/privee/gestion-page-form.php
+++ projet/vue/privee/gestion-page-form.php
@@ -4,7 +4,20 @@
         <?php require "lib/menu-privee.php" ?>
     </div>
     <div class="col">
-    
-   
+        <!-- 
+            dans la page php gestion-page-form.php 
+            créer un formulaire qui contient 5 champs 
+            utilisez les noms des colonnes de la table pages 
+            comme name="" de chaque champ 
+            titre input text
+            slug  => input text 
+            contenu => textarea
+            image => input text 
+            auteur => champ de type text
+
+            mettre en forme le formulaire via la librairie bootstrap 
+
+            bonus (ensemble) => transformer le champ textarea rich via la librairie tinyMCE (ensemble)
+        -->
     </div>
 </section>
